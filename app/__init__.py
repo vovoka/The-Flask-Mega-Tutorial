00c8bd086a49b7448afa9957440dcce4e2ea0aaa
v.kovalov <v.kovalov@smartweb.com.ua>
2020-02-04 10:55:20 +0200

    Chapter_11: Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 
 from config import Config
 # Create app & configure it
@@ -21,6 +23,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
